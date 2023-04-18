94f91e3e1b11cca1ec55f72ff9768a9093b02199
jacob-milton <jrmilton@lbl.gov>
2023-04-18 12:55:12 -0700

    more fun with tuples

--- HiPRGen/extract_reactions.py
+++ HiPRGen/extract_reactions.py
@@ -79,15 +79,11 @@ for reaction in first_entries["pathways"].keys():
                                     side[species_index] = species_hash
                     for side in participants:
                         side.sort()
-                        side = tuple(side)
-                        print(side)
-                        print(type(side))
+                        side_tuple = tuple(side)
+                        side_index = participants.index(side)
+                        participants[side_index] = side_tuple
                     reaction_dict = {}
                     participants = tuple(participants)
-                    print(participants)
-                    print(type(participants))
-                    print(sum(reaction_charges))
-                    print(type(sum(reaction_charges)))
                     reaction_dict[participants] = sum(reaction_charges)
                     if not resonant_reaction(reaction_dict, added_hashes):
                         added.append(reaction)
