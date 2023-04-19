d51450fbe419496b4845f966c47aecca03b1978e
jacob-milton <jrmilton@lbl.gov>
2023-04-18 22:00:12 -0700

    seem to have fixed everything

--- HiPRGen/extract_reactions.py
+++ HiPRGen/extract_reactions.py
@@ -4,31 +4,10 @@ Created on Fri Apr 14 13:13:41 2023
 
 @author: JRMilton
 """
-from monty.serialization import loadfn
+from monty.serialization import loadfn, dumpfn
 import operator
 import pickle
 
-# #Provide Evan with a list of the form:
-
-# [{"reactants":[MPCuleID_A, MPCuleID_B], "products":[MPCuleID_C, MPCuleID_D]}, etc]
-
-
-# 1. all non-electron-containing from phase 1 tally
-# 2. for each network product, 10 most abundant one-step pathways
-# 3. all non-charge-transfer from phase 2 tally with >= 500 occurences
-
-# from all of those, ensure no duplicates + no duplicates modulo species charge
-
-# (A + B- -> C- + D
-# A- + B -> C- + D
-# A- + B -> C + D-
-# A + B- -> C + D- 
-
-# Only one reaction!)
-
-
-# total should be less than 7000
-# JSONs contain reaction IDs, rn.sqlite contains mapping from reaction ID to species IDs, mol_entries.pickle contains mapping from species IDs to 
 mpcule_ids = []
 added =[]
 added_hashes = {}
@@ -46,10 +25,55 @@ def resonant_reaction(reaction_dict, added_hashes):
     for reaction in added_hashes.keys():  #for each reaction currently in mpculids,
         for rxn in reaction_dict.keys():
             if rxn == reaction: #compare the reactant hashes of the new reaction and the old one
-                print('same graphs')
-                if added_hashes[reaction] == int(reaction_dict.values()): #if they're the same, compare the product hashes
-                    print('fired')
+                if added_hashes[reaction] == reaction_dict[rxn]: #if they're the same, compare the product hashes
                     return True
+
+    return False
+
+def create_reaction_dict(participants):
+    reaction_charges = []
+    for side in participants:
+        for species in side: #takes the list of ids, finds their corresponding mol_entries, which have their charges and hashes
+            for mol_entry in mol_entries:
+                m_id = mol_entry.entry_id
+                if m_id == species:
+                    species_charge = mol_entry.charge
+                    reaction_charges.append(species_charge)
+                    species_hash = mol_entry.covalent_hash
+                    species_index = side.index(species)
+                    side[species_index] = species_hash
+    for side in participants:
+        side.sort()
+        side_tuple = tuple(side)
+        side_index = participants.index(side)
+        participants[side_index] = side_tuple
+    reaction_dict = {}
+    participants = tuple(participants)
+    reaction_dict[participants] = sum(reaction_charges)
+    return reaction_dict
+
+def reverse_reaction(reaction_dict, added_hashes):
+    for reaction in reaction_dict.keys():
+        reverse = (reaction[1], reaction[0])
+        for rxn in added_hashes:
+            if reverse == rxn and reaction_dict[reaction] == added_hashes[rxn]:
+                return True
+
+    return False
+
+def charge_transfer_reaction(reaction_dict):
+    for reaction in reaction_dict.keys():
+        reactant_total_hashes = set(reaction[0])
+        product_total_hashes = set(reaction[1])
+        reactant_hash_list = list(reaction[0])
+        product_hash_list = list(reaction[1])
+
+    if len(reactant_total_hashes.intersection(product_total_hashes)) == 2:
+        return True
+    elif len(reactant_total_hashes.intersection(product_total_hashes)) == 1 and reactant_hash_list[0] == reactant_hash_list[1] and product_hash_list[0] == product_hash_list[1]:
+        return True
+    else:
+        return False
 
     return False
 
@@ -66,74 +90,74 @@ for reaction in first_entries["pathways"].keys():
                 if reaction not in added:
                     reactants = first_entries["reactions"][rxn]["reactants"]
                     products = first_entries["reactions"][rxn]["products"]
-                    participants = [reactants, products] #think I'm adding a mysterious comma here
-                    reaction_charges = []
-                    for side in participants:
-                        for species in side: #takes the list of ids, finds their corresponding mol_entries, which have their charges and hashes
-                            for mol_entry in mol_entries:
-                                m_id = mol_entry.entry_id
-                                if m_id == species:
-                                    species_charge = mol_entry.charge
-                                    reaction_charges.append(species_charge)
-                                    species_hash = mol_entry.covalent_hash
-                                    species_index = side.index(species)
-                                    side[species_index] = species_hash
-                    for side in participants:
-                        side.sort()
-                        side_tuple = tuple(side)
-                        side_index = participants.index(side)
-                        participants[side_index] = side_tuple
-                    reaction_dict = {}
-                    participants = tuple(participants)
-                    reaction_dict[participants] = sum(reaction_charges)
+                    participants = [reactants, products] 
+                    reaction_dict = create_reaction_dict(participants)
                     if not resonant_reaction(reaction_dict, added_hashes):
                         added.append(reaction)
                         mpcule_ids.append(first_entries["reactions"][rxn])
-                        added_hashes[participants] = sum(reaction_charges)
+                        added_hashes.update(reaction_dict.items())
                         
 print('Done! ', len(mpcule_ids), ' added')
 
 
-# print('Adding reactions for phase 2 network products...')                                             
-# second_name = 'sink_report'
-# second_entries = loadfn(second_name + ".json") #loads json as a dictionary whose keys are mol ids and values are
-#                                                 #dictionaries whose keys are labels of values
-# print(len(second_entries.keys()))
-# for dictionary in second_entries.values():
-#     species_index = dictionary["species_index"]
-#     reaction_json = loadfn(str(species_index) + "_pathway.json") #dictionary containing two keys: pathways and reactions. 
-#     pathways = reaction_json["pathways"]
-#     pathways.sort(key = operator.itemgetter('frequency'), reverse = True)
-#     top_pathways = []
-#     n = 1
-#     ten_saved = False
+print('Adding reactions for phase 2 network products...')                                             
+second_name = 'sink_report'
+second_entries = loadfn(second_name + ".json") #loads json as a dictionary whose keys are mol ids and values are
+                                                #dictionaries whose keys are labels of values
+for dictionary in second_entries.values():
+    species_index = dictionary["species_index"]
+    reaction_json = loadfn(str(species_index) + "_pathway.json") #dictionary containing two keys: pathways and reactions. 
+    pathways = reaction_json["pathways"]
+    pathways.sort(key = operator.itemgetter('frequency'), reverse = True)
+    top_pathways = []
+    n = 1
+    ten_saved = False
     
-#     while not ten_saved:
-#         for dictionary in pathways:
-#             if int(dictionary['weight']) == n:
-#                 top_pathways.append(dictionary['pathway'])
-#                 if len(top_pathways) == 10:
-#                     ten_saved = True
-#                     break
-#         n += 1
+    while not ten_saved:
+        for dictionary in pathways:
+            if int(dictionary['weight']) == n:
+                top_pathways.append(dictionary['pathway'])
+                if len(top_pathways) == 10:
+                    ten_saved = True
+                    break
+        n += 1
     
-#     for rxn in top_pathways:
-#         for reaction in rxn:
-#             for num in reaction_json["reactions"].keys():
-#                 if num == str(reaction):
-#                     mpcule_ids.append(reaction_json["reactions"][num])
-#     n = 1
+    for rxn in top_pathways:
+        for reaction in rxn:
+            for num in reaction_json["reactions"].keys():
+                if num == str(reaction):
+                    reactants = reaction_json["reactions"][num]["reactants"]
+                    products = reaction_json["reactions"][num]["products"]
+                    participants = [reactants, products] 
+                    reaction_dict = create_reaction_dict(participants)
+                    if not resonant_reaction(reaction_dict, added_hashes):
+                        if not reverse_reaction(reaction_dict, added_hashes):
+                            if not charge_transfer_reaction(reaction_dict):
+                                mpcule_ids.append(reaction_json["reactions"][num])
+                                added.append(num)
+                                added_hashes.update(reaction_dict.items())
+    n = 1
                 
-# print('Done! ', len(mpcule_ids), ' reactions total')
+print('Done! ', len(mpcule_ids), ' reactions total')
 
-# print('Adding reactions for phase 2 tally...')  
-# third_name = 'reaction_tally'
-# third_entries = loadfn(third_name + ".json")
+print('Adding reactions for phase 2 tally...')  
+third_name = 'reaction_tally'
+third_entries = loadfn(third_name + ".json")
 
-# for reaction in third_entries["pathways"].keys():
-#     if third_entries["pathways"][reaction] > 500:
-#         for rxn in third_entries["reactions"].keys():
-#             if str(reaction) == rxn:
-#                 mpcule_ids.append(third_entries["reactions"][rxn])
+for reaction in third_entries["pathways"].keys():
+    if third_entries["pathways"][reaction] > 500:
+        for rxn in third_entries["reactions"].keys():
+            if str(reaction) == rxn:
+                reactants = third_entries["reactions"][reaction]["reactants"]
+                products = third_entries["reactions"][reaction]["products"]
+                participants = [reactants, products] 
+                reaction_dict = create_reaction_dict(participants)
+                if not resonant_reaction(reaction_dict, added_hashes):
+                    if not reverse_reaction(reaction_dict, added_hashes):
+                        if not charge_transfer_reaction(reaction_dict):
+                            mpcule_ids.append(third_entries["reactions"][reaction])
+                            added.append(reaction)
+                            added_hashes.update(reaction_dict.items())
 
-# print('Done! ', len(mpcule_ids), ' reactions total')
+print('Done! ', len(mpcule_ids), ' reactions total')
+dumpfn(mpcule_ids, 'euvl_TSreactions_041823.json')
